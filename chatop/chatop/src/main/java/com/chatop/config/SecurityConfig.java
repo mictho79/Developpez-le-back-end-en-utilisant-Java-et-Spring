cc8654a76c955c9e5e5aa29d93fb64e18f770f06
Michael Thomas <mic.thomas@sfr.fr>
2025-05-30 12:16:56 +0200

    nettoyage

--- chatop/chatop/src/main/java/com/chatop/config/SecurityConfig.java
+++ chatop/chatop/src/main/java/com/chatop/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     config.setAllowedOrigins(List.of("http://localhost:4200"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("*"));
-    config.setExposedHeaders(List.of("Authorization")); 
+    config.setExposedHeaders(List.of("Authorization"));
     config.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
